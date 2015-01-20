6e3257d2a8874491f9ccae4630a46455f06a7881
Anthony To <to.anthony93@gmail.com>
2015-01-19 19:45:41 -0600

    Add find location to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,15 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :find_location
   
   
   def after_sign_in_path_for(user)
     user_path(current_user.id)
   end
 
+  def find_location
+    @location = request.location
+  end
+  
 end
