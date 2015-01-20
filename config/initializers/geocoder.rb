3dd66b320588fb8894354dd86de337b62fb7f858
Anthony To <to.anthony93@gmail.com>
2015-01-19 19:59:54 -0600

    Remove geocoder timeout

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -10,7 +10,7 @@ Geocoder.configure(
   # :api_key => "...",
 
   # geocoding service request timeout, in seconds (default 3):
-  :timeout => 30
+  # :timeout => 30,
 
   # set default units to kilometers:
   # :units => :km,
