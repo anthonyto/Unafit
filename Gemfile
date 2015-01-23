7319e905fb9801f3cd088f758afd9aa89649aa22
Anthony To <to.anthony93@gmail.com>
2015-01-23 17:30:00 -0600

    Styling

--- Gemfile
+++ Gemfile
@@ -43,8 +43,6 @@ gem 'geocoder'
 gem "paperclip", "~> 4.2"
 gem 'aws-sdk'
 
-
-
 group :development, :test do 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
@@ -53,4 +51,6 @@ group :development, :test do
   gem 'foreman'
 end
 
-
+group :production do
+  gem 'rails_12factor'
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :edit_subscriptions, :destroy]
   before_action :authenticate_user!
-  before_filter :authorize_user
+  # before_filter :authorize_user
 
   # GET /users
   # GET /users.json
@@ -54,44 +54,44 @@ class UsersController < ApplicationController
   end
 
   # I don't think we need these
-  # # GET /users/new
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # # POST /users
-  # # POST /users.json
-  # def create
-  #   @user = User.new(user_params)
-  #
-  #   respond_to do |format|
-  #     if @user.save
-  #       format.html { redirect_to @user, notice: 'User was successfully created.' }
-  #       format.json { render :show, status: :created, location: @user }
-  #     else
-  #       format.html { render :new }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-  #
-  # # GET /users/1/edit
-  # def edit
-  # end
-  #
-  # # PATCH/PUT /users/1
-  # # PATCH/PUT /users/1.json
-  # def update
-  #   respond_to do |format|
-  #     if @user.update(user_params)
-  #       format.html { redirect_to @user, notice: 'User was successfully updated.' }
-  #       format.json { render :show, status: :ok, location: @user }
-  #     else
-  #       format.html { render :edit }
-  #       format.json { render json: @user.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+  end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
   
   # GET /users/1/edit_subscriptionss
   def edit_subscriptions
